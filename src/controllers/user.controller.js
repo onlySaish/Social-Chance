88282d16fc8c7fd0bfc337bf30de5db9bdcd2d4f
onlySaish <onlysainok24@gmail.com>
2024-12-15 22:09:43 +0530

    auth middleware & User login, logout in controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,22 @@ import ApiError from "../utils/ApiError.js";
 import {asyncHandler} from "../utils/asyncHandler.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+
+const generateAccessAndRefreshToken = async (userId) => {
+    try {
+        const user = await User.findById(userId);
+        const accessToken = await user.generateAccessToken();
+        const refreshToken = await user.generateRefreshToken();
+        
+        user.refreshToken = refreshToken;
+        await user.save({validateBeforeSave : false})
+
+        return {accessToken,refreshToken};        
+    } catch (error) {
+        throw new ApiError(500,"Some Error Occured while generating Access / Refresh Tokens")
+    }
+}
+
 const userRegister = asyncHandler(async(req,res) => {
     const {fullName, username, email, password} = req.body    //Get values from frontend
     
@@ -50,4 +66,94 @@ const userRegister = asyncHandler(async(req,res) => {
     )
 })
 
-export {userRegister};
+const userLogin = asyncHandler(async(req,res) => {
+    //Get email, usename and pass from user (frontend)
+    //Validate no fields are left empty
+    //Search for email or username in user database
+    //If user not found then throw err no user
+    //If user found compare the input password and password stored in database using bcrypt compare
+    //If password does not match throw err
+    //If pass is correct Fetch the user data from database
+    //Generate Access Token and Refresh Token for user
+    //Save the genrated refresh token in db
+    //get info of loggedinUser with removeal of unnecessary keys
+    //Send tokens in form of cookie
+    //return reponse(ApiResponse) i.e. user data
+    
+    const {email, username, password} = req.body
+
+    if (
+        [email, username, password].some((field) => {
+            field === ""
+        })
+    )   throw new ApiError(404, "All Fields are Required");
+
+    const user = await User.findOne({
+        $or : [{username}, {email}]
+    });
+
+    if (!user) {throw new ApiError(400, "User with Username or Email Not Found")};
+
+    const isPasswordValid = await user.isPasswordCorrect(password);
+
+    if(!isPasswordValid) {throw new ApiError(404,"Incorrect Password, Try Again !!")}
+
+    const {accessToken, refreshToken} = await generateAccessAndRefreshToken(user._id);
+
+    const loggedinUser = await User.findById(user._id).select("-password -refreshToken");
+
+    const options = {
+        httpOnly: true,
+        secure: true 
+    }
+
+    res.status(200)
+    .cookie("accessToken",accessToken,options)
+    .cookie("refreshToken",refreshToken,options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user: loggedinUser, accessToken, refreshToken     
+            },
+            "Logged In Successfully"
+        )
+    )
+})
+
+const userLogout = asyncHandler(async(req,res) => {
+    await User.findByIdAndUpdate(req.user._id,
+        {
+            $set: {
+                refreshToken : undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken",options)
+    .clearCookie("refreshToken",options)
+    .json(
+        new ApiResponse(
+            201,
+            {},
+            "Logged Out Successfully"
+        )
+    )
+
+})
+
+export {
+    userRegister,
+    userLogin,
+    userLogout
+};
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { userRegister } from "../controllers/user.controller.js";
+import { userLogin, userLogout, userRegister } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
@@ -16,5 +17,10 @@ router.route("/register").post(
         }
     ])
 ,userRegister);
+
+router.route("/login").post(userLogin);
+
+//Secure Routes
+router.route("/logout").post(verifyJWT, userLogout);
 
 export default router;
